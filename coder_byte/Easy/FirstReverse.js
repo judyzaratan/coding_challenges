2848b2b783d89e086778185109ae03adfd7cb1c6
mochicat8 <mochicat8@gmail.com>
2015-10-14 23:14:45 -0700

    changes

--- coder_byte/Easy/FirstReverse.js
+++ coder_byte/Easy/FirstReverse.js
@@ -24,12 +24,12 @@ console.log(FirstReverse('Coderbyte'));
 //Recursive
 function FirstReverse(str){
   var recurse = function(workingStr, reverse, count){
-    console.log(count);
     if(reverse.length === str.length){
       return reverse;
     }
     return recurse(workingStr.substring(0, workingStr.length-1), reverse + workingStr[workingStr.length-1], count+1);
   };
-  return recurse(str, "", 0);
+  var output = recurse(str, "", 0);
+  return output;
 }
 console.log(FirstReverse("testing"));
