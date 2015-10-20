fe691bc1e6223d6ac6b1034f33fe98061d1e3d8d
mochicat8 <mochicat8@gmail.com>
2015-10-19 22:27:52 -0700

    adds problems

---
+++ jsDude_interview/13_ifPalindrome.js
@@ -0,0 +1,9 @@
+function checkPalindrome(str){
+//iterate through string
+//have two pointers start and end
+//find midpoint
+//iterate to midpoint
+// if two pointers do not equal each other
+
+
+}
